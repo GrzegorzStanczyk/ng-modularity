b8c0c3cb4db3f6d2a9ec65d3f82b6e3de3209730
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2019-03-09 16:14:15 +0100

    flat routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,48 +13,37 @@ import { RouterModule } from '@angular/router';
     RouterModule.forRoot([
       {
         path: '',
-        redirectTo: '/load/(feature:feature//feature1:feature1//feature2:feature2//feature3:feature3//feature4:feature4)',
-        pathMatch: 'full',
+        redirectTo: '/(feature:feature/v3//feature1:feature1//feature2:feature2//feature3:feature3/v2//feature4:feature4)',
+        pathMatch: 'full'
+      },
+      {
+        path: 'feature',
+        loadChildren: './feature/feature.module#FeatureModule',
+        outlet: 'feature'
+      },
+      {
+        path: ':feature1',
+        loadChildren: './feature1/feature1.module#Feature1Module',
+        outlet: 'feature1'
+      },
+      {
+        path: ':feature2',
+        loadChildren: './feature2/feature2.module#Feature2Module',
+        outlet: 'feature2'
+      },
+      {
+        path: ':feature3',
+        loadChildren: './feature3/feature3.module#Feature3Module',
+        outlet: 'feature3'
+      },
+      {
+        path: ':feature4',
+        loadChildren: './feature4/feature4.module#Feature4Module',
+        outlet: 'feature4'
       },
       {
         path: 'login',
         loadChildren: './login/login.module#LoginModule',
-      },
-      {
-        path: 'load',
-        component: AppComponent,
-        children: [
-          {
-            path: '',
-            redirectTo: '/load/(feature:feature//feature1:feature1//feature2:feature2//feature3:feature3//feature4:feature4)',
-            pathMatch: 'full'
-          },
-          {
-            path: 'feature',
-            loadChildren: './feature/feature.module#FeatureModule',
-            outlet: 'feature'
-          },
-          {
-            path: ':feature1',
-            loadChildren: './feature1/feature1.module#Feature1Module',
-            outlet: 'feature1'
-          },
-          {
-            path: ':feature2',
-            loadChildren: './feature2/feature2.module#Feature2Module',
-            outlet: 'feature2'
-          },
-          {
-            path: ':feature3',
-            loadChildren: './feature3/feature3.module#Feature3Module',
-            outlet: 'feature3'
-          },
-          {
-            path: ':feature4',
-            loadChildren: './feature4/feature4.module#Feature4Module',
-            outlet: 'feature4'
-          },
-        ]
       },
     ])
   ],
